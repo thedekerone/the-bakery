b34be2b446bd68f8d8a83608573ce71fb5c302c8
Mauricio Fow <mauriciofow@gmail.com>
2019-07-12 13:16:42 -0500

    Updates

---
+++ precache-manifest.ece937c857d800580f04387df7658d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1931e9c866f4564f62fd986a5a64f58",
+    "url": "/thedekerone/the-bakery/index.html"
+  },
+  {
+    "revision": "40faeef386456d423350",
+    "url": "/thedekerone/the-bakery/static/css/main.54eb5438.chunk.css"
+  },
+  {
+    "revision": "97a889896c584d3fe1bc",
+    "url": "/thedekerone/the-bakery/static/js/2.f7c17a78.chunk.js"
+  },
+  {
+    "revision": "40faeef386456d423350",
+    "url": "/thedekerone/the-bakery/static/js/main.d3fea208.chunk.js"
+  },
+  {
+    "revision": "5585b85dd9c2daf59337",
+    "url": "/thedekerone/the-bakery/static/js/runtime~main.74fe8119.js"
+  },
+  {
+    "revision": "a57e022ee414c09ce00ca129e32f4ba9",
+    "url": "/thedekerone/the-bakery/static/media/cakes2.a57e022e.jpg"
+  }
+]);
